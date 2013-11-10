8ec5e1fa3d4639c45a7a623d994c090063855105
Tomer Filiba <tomer.filiba@emc.com>
2013-11-10 20:36:43 +0200

    models and work plan

--- schedup/models.py
+++ schedup/models.py
@@ -1,7 +1,45 @@
 from google.appengine.ext import ndb
+from datetime import timedelta
 
 
 class UserProfile(ndb.Model):
     email = ndb.StringProperty(required=True)
+    google_id = ndb.StringProperty()
+    facebook_id = ndb.StringProperty()
+
+    def get_owner_events(self):
+        return EventInfo.query(EventInfo.owner == self.key)
+    
+    def get_participating_events(self):
+        return EventInfo.query(EventInfo.recepient_emails == self.email)
+
+
+class EventInfo(ndb.Model):
+    owner = ndb.KeyProperty(UserProfile, required = True)
+    recepients = ndb.KeyProperty(UserProfile, repeated = True)
+    confirmed = ndb.KeyProperty(UserProfile, repeated = True)
+    tokens = ndb.StringProperty(repeated = True)
+
+    title = ndb.StringProperty()    
+    start_time = ndb.DateTimeProperty(required = True)
+    duration_minutes = ndb.IntegerProperty(required = True)
+
+    @property
+    def end_time(self):
+        return self.start_time + timedelta(minutes = self.duration_minutes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- schedup/views.py
+++ schedup/views.py
@@ -1,4 +1,5 @@
 from schedup.base import BaseHandler
+from schedup.models import EventInfo
 
 
 class MainPage(BaseHandler):
@@ -10,12 +11,68 @@ class MainPage(BaseHandler):
             subtitle = "The way to schedule up", 
             content = "hello moishe")
 
-class FooPage(BaseHandler):
-    URL = "/foo"
+# Tomer + send emails
+class LoginPage(BaseHandler):
+    URL = "/login"
     
     def get(self):
-        self.render_response('index.html', 
-            title = "SchedUp", 
-            subtitle = "The way to schedule up", 
-            content = "hello baruch")
+        self.render_response('index.html')
 
+# Shir
+class MyEventsPage(BaseHandler):
+    URL = "/myevents"
+    
+    def get(self):
+        EventInfo.get_owner_events()
+        self.render_response('index.html')
+
+# Shir
+class InvitedToPage(BaseHandler):
+    URL = "/invited"
+    
+    def get(self):
+        EventInfo.get_participating_events()
+        self.render_response('index.html')
+
+# Ofir
+class NewEventPage(BaseHandler):
+    URL = "/new"
+    
+    def post(self):
+        evt = EventInfo(title = self.request["title"], start_date = self.request["start_date"])
+        evt.token = "random token"
+        evt.save()
+
+# Yana
+class GuestPage(BaseHandler):
+    URL = "/guest/(.*)"
+    
+    def get(self, token):
+        evt = EventInfo.query(token == token).get()
+        self.render_response('index.html')
+    
+    def post(self, token):
+        evt = EventInfo.query(token == token).get()
+        if self.request["answer"]:
+            evt.confirmed.append(token)
+
+
+
+'''
+
+MVC - model view container
+1. login with google account, ask permissions for calendar, email, (contacts?)
+2. finish models 
+3. create simple event form
+4. my meetings page/my invites page
+5. recipient page (yes/no)
+
+
+Hello <>
+Moshe invited you to a meeting.
+<Click here http://schedup.appspot.com/guest/983329847376783745>
+    
+'''
+        
+      
+      
