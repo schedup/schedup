9b550d560bc8ee8d94525c28fe522232b8309f62
ofirventura <ofirventura@gmail.com>
2013-11-17 10:38:47 +0200

    Adding Files to project - to add later the "new event" page
    still does not run perfectly so I upload only the necessary files but
    not the class itself
    
    my events page
    
    jquery calendar + new event post handler

--- schedup/models.py
+++ schedup/models.py
@@ -16,27 +16,18 @@ class UserProfile(ndb.Model):
 
 
 class EventInfo(ndb.Model):
-
     owner = ndb.KeyProperty(UserProfile, required = True)
     recepients = ndb.KeyProperty(UserProfile, repeated = True)
     confirmed = ndb.KeyProperty(UserProfile, repeated = True)
     tokens = ndb.StringProperty(repeated = True)
-
-    title = ndb.StringProperty()    
-    start_time = ndb.DateTimeProperty(required = True)
-    duration_minutes = ndb.IntegerProperty(required = True)
-        
-    @property
-    def end_time(self):
-        return self.start_time + timedelta(minutes = self.duration_minutes)
-
-
-
-
-
-
-
-
+    daytime = ndb.StringProperty(repeated = True)
+    type = ndb.StringProperty()
+    title = ndb.StringProperty(required = True)    
+    description = ndb.StringProperty()
+    start_window = ndb.DateProperty(required = True)
+    end_window = ndb.DateProperty(required = True)
+    start_time = ndb.DateTimeProperty()
+    end_time = ndb.DateTimeProperty()
 
 
 
--- schedup/views.py
+++ schedup/views.py
@@ -1,3 +1,7 @@
+import string
+import random
+import logging
+from schedup.base import BaseHandler, oauth
 from schedup.base import BaseHandler, oauth, logged_in
 from datetime import datetime, timedelta
 from schedup.connector import GoogleConnector
@@ -15,10 +19,12 @@ class MainPage(BaseHandler):
     URL = "/"
     
     def get(self):
-        self.render_response('index.html', 
-            title = "SchedUp", 
-            subtitle = "The way to schedule up", 
-            content = "hello moishe")
+        flash =self.session.pop("msg", "")
+        self.render_response('index.html',
+            title="SchedUp",
+            flash=flash,
+            subtitle="The way to schedule up",
+            content="hello moishe")
 
 class CalPage(BaseHandler):
     URL = "/calendar"
@@ -32,53 +38,64 @@ class CalPage(BaseHandler):
             timeMax = today + timedelta(days = 7)).execute(http=oauth.http())
         self.render_response('index.html', content = repr(events))
 
-class ContactPage(BaseHandler):
-    URL = "/contact"
-    
-    @oauth.oauth_required
-    def get(self):
-        from gdata.contacts.client import ContactsClient, ContactsQuery
-        from gdata.gauth import ClientLoginToken
-        
-        client = ContactsClient(source = "SchedUp", auth_token = ClientLoginToken(oauth.credentials.access_token))
-        query = ContactsQuery(max_results = 10)
-        result = client.GetContacts(q=query)
-        self.render_response('index.html', content = repr(result))
+        calendar_service = discovery.build('calendar', 'v3')
+        result = calendar_service.calendarList().list().execute(http=oauth.http())
+        self.render_response('index.html', content=repr(result))
 
+class NewEventPage(BaseHandler):
+    URL = "/new"
+    
+    def post(self):
+        date_start_window=datetime.strptime(self.request.get("start"), "%d/%m/%y").date()
+        date_end_window=datetime.strptime(self.request.get("end"), "%d/%m/%y").date()      
+        
+        user = UserProfile.query(UserProfile.email == "kaka").get()
+        if not user:
+            user = UserProfile(email="kaka")
+            user.put()
+        tok = id_generator()
+        evt = EventInfo(owner=user.key,title=self.request.get("title"),description=self.request.get("description"),
+                        start_window=date_start_window, end_window=date_end_window, tokens=[tok] )
+        evt.put()
+        
+        
+        # todo:
+        # how to connect between the submit button + how does the save work 
+        # use jQuery to save to database
+        # use DOM interface to add recipients list
+        # validate input - mails , valid time window
+        # not allowing too long inputs
+        logging.info("token=%s", tok)        
+        self.session["msg"] = "hello" + evt.title
+        self.redirect("/")
 
 class ProfilePage(BaseHandler):
     URL = "/profile"
     
-    @logged_in
+    @logged_in       
     def get(self):
         self.render_response('index.html', content = repr(self.user.email))
+            self.render_response('newevent.html')
 
+
+def id_generator(size=20, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+       
 
 # Shir
 class MyEventsPage(BaseHandler):
     URL = "/myevents"
     
     def get(self):
-        self.render_response('myevents.html',events = my_events,active="events")
+        self.render_response('myevents.html', events = my_events,active="events")
 
 # Shir
 class InvitedToPage(BaseHandler):
     URL = "/invited"
     
     def get(self):
-        self.render_response('invitedto.html',events = my_events, active="events")
+        self.render_response('invitedto.html', events = my_events, active="events")
 
-
-'''
-# Ofir
-class NewEventPage(BaseHandler):
-    URL = "/new"
-    
-    def post(self):
-        evt = EventInfo(title = self.request["title"], start_date = self.request["start_date"])
-        evt.token = "random token"
-        evt.save()
-'''
 
 class FooPage(BaseHandler):
     URL = "/foo"
@@ -110,19 +127,4 @@ class GuestPage(BaseHandler):
             evt.confirmed.append(token)
 
 
-'''
-
-MVC - model view container
-1. login with google account, ask permissions for calendar, email, (contacts?)
-2. finish models 
-3. create simple event form
-4. my meetings page/my invites page
-5. recipient page (yes/no)
-
-
-Hello <>
-Moshe invited you to a meeting.
-<Click here http://schedup.appspot.com/guest/983329847376783745>
-    
-'''
 
