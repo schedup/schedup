f75e358d3491f6a0835dab41f56bea64f0421112
Tomer Filiba <tomerfiliba@gmail.com>
2013-11-23 20:41:54 +0200

    change email

--- schedup/settings.py
+++ schedup/settings.py
@@ -1,4 +1,4 @@
-EMAIL_ADDRESS = "admin@oval-replica-393.appspotmail.com"
+EMAIL_ADDRESS = "SchedUp <admin@oval-replica-393.appspotmail.com>"
 CLIENT_ID = '945595987955.apps.googleusercontent.com'
 CLIENT_SECRET = 'AB5nX4EntL2Mv2ahCDoKE-EF'
 SESSION_SECRET = 'kuOVSF9+DWJdxFTHU0+ayJPo86Nt77DnGz2bz1x7b5gNyTbgEY9kcu8o7vpBBGulVMD9IWqcvYkt'
