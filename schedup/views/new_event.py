02641b52a985daa4cd1762587c3d539f9326b403
ofirventura <ofirventura@gmail.com>
2013-11-23 14:28:20 +0200

    adding errors to new event page

--- schedup/views/new_event.py
+++ schedup/views/new_event.py
@@ -6,6 +6,8 @@ import random
 from dateutil.parser import parse as parse_datetime
 from schedup.base import BaseHandler, logged_in
 from schedup.models import UserProfile, EventInfo, EventGuest
+from datetime import datetime
+from xmlrpclib import DateTime
 
 
 class NewEventPage(BaseHandler):
@@ -13,12 +15,12 @@ class NewEventPage(BaseHandler):
     TOKEN_SIZE = 20
     
     @classmethod
-    def generate_token(cls, chars = string.ascii_letters + string.digits):
+    def generate_token(cls, chars=string.ascii_letters + string.digits):
         return ''.join(random.choice(chars) for _ in range(cls.TOKEN_SIZE))
 
     @logged_in
     def get(self):
-        return self.render_response("new_event.html", post_url = self.URL)
+        return self.render_response("new_event.html", post_url=self.URL)
 
     @logged_in
     def post(self):
@@ -33,25 +35,25 @@ class NewEventPage(BaseHandler):
         for email in self.request.params["guests"].split(";"):
             user = UserProfile.query(UserProfile.email == email).get()
             if user:
-                gst = EventGuest(user = user.key, token = self.generate_token())
+                gst = EventGuest(user=user.key, token=self.generate_token())
             else:
-                gst = EventGuest(email = email, token = self.generate_token())
+                gst = EventGuest(email=email, token=self.generate_token())
             guests.append(gst)
-        
         owner_token = self.generate_token()
-        evt = EventInfo(owner = self.user.key, 
-            owner_token = owner_token,
-            title = self.request.params["title"],
-            daytime = daytime,
-            type = self.request.params.get("type"),
-            start_window = parse_datetime(self.request.params["fromdate"]),
-            end_window = parse_datetime(self.request.params["todate"]),
-            guests = guests,
+        evt = EventInfo(owner=self.user.key,
+            owner_token=owner_token,
+            title=self.request.params["title"],
+            daytime=daytime,
+            type=self.request.params.get("type"),
+            start_window=parse_datetime(self.request.params["fromdate"]),
+            end_window=parse_datetime(self.request.params["todate"]),
+            guests=guests,
+            description=self.request.params["description"],
         )
         evt.put()
         
         self.redirect_with_flashmsg("/my", "Event created successfully")
-        #return self.render_response("calendar.html", title = "Choose Time Slots", 
+        # return self.render_response("calendar.html", title = "Choose Time Slots", 
         #    post_url = "/choose/%s" % (owner_token,))
 
 
