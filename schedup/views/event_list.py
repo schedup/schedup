fb44fd9522b80473c36c366d94744eafbe6a02e9
Tomer Filiba <tomer.filiba@emc.com>
2013-12-29 18:52:53 +0200

    start and end window alignment

--- schedup/views/event_list.py
+++ schedup/views/event_list.py
@@ -3,6 +3,7 @@ Events I organize or that I am invited to
 """
 from schedup.base import BaseHandler, logged_in
 from schedup.models import EventInfo
+from datetime import datetime, timedelta
 
 
 class MyEventsPage(BaseHandler):
@@ -11,11 +12,14 @@ class MyEventsPage(BaseHandler):
     @logged_in
     def get(self):
         token = self.session.pop("token", None)
+        mintime = datetime.now() - timedelta(days = 3)
         self.render_response("event_list.html", 
             title = "My Events",
             owner = True,
-            events = [evt for evt in self.user.get_owner_events().order(-EventInfo.created_at) if evt.status != "canceled"],
-            token = token, 
+            events = [evt for evt in 
+                self.user.get_owner_events().filter(EventInfo.end_window >= mintime).order(EventInfo.end_window) 
+                if evt.status != "canceled" and (not evt.end_time or evt.end_time >= mintime)],
+            token = token,
         )
 
 class InvitedToPage(BaseHandler):
@@ -23,9 +27,12 @@ class InvitedToPage(BaseHandler):
     
     @logged_in
     def get(self):
+        mintime = datetime.now() - timedelta(days = 3)
         self.render_response("event_list.html", 
             title = "Invited To", 
-            events = [evt for evt in self.user.get_participating_events() if evt.status != "canceled"], 
+            events = [evt for evt in 
+                self.user.get_participating_events().filter(EventInfo.end_window >= mintime).order(EventInfo.end_window) 
+                if evt.status != "canceled" and (not evt.end_time or evt.end_time >= mintime)],
             owner = False,
         )
 
